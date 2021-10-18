29bf454d697955741e242b67a863c6238048d3f7
Nishimura <yukinkopen@gmail.com>
2021-10-18 12:43:45 +0900

    [mod] 設計用のリンクを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -130,6 +130,132 @@ const Header = () => (
               </li>
             </ul>
           </li>
+          <li className="nav-item dropdown link-secondary">
+            <a
+              className="nav-link dropdown-toggle link-secondary"
+              href="http://www.google.com/"
+              id="dropdown01"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              data-tip="本アプリの設計情報リンク（開発者）"
+            >
+              <i className="fas fa-user-cog" /> 設計情報
+              <ReactTooltip effect="float" type="dark" place="bottom" />
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="dropdown01">
+              <li>
+                <h6 className="dropdown-header">準備資料</h6>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="https://docs.google.com/document/d/1-kFU7rRkBtfA0IBggwJ2HuQGoURDMNoelnOfSnYKO_o/edit?usp=sharing"
+                  data-tip="主にチャンネルでの情報をメモ（開発者）"
+                >
+                  <i className="fas fa-running" /> 課題覚え書き
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="https://drive.google.com/file/d/1mMyTRoarZ3Y-5BUZrI_eVyJYuUOmT9Ud/view?usp=sharing"
+                  data-tip="配布された課題PDF（開発者）"
+                >
+                  <i className="fas fa-running" /> 課題定義書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="ヒアリング結果を記載（開発者）"
+                >
+                  <i className="fas fa-running" /> ヒアリング報告書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <h6 className="dropdown-header">設計資料</h6>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="顧客要求を整理（開発者）"
+                >
+                  <i className="fas fa-running" /> 要求仕様書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="要求を基に開発要件を作成（開発者）"
+                >
+                  <i className="fas fa-running" /> 要件定義書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="システムの基本仕様（開発者）"
+                >
+                  <i className="fas fa-running" /> 基本設計仕様書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="システムの詳細仕様（開発者）"
+                >
+                  <i className="fas fa-laptop" /> 詳細設計仕様書
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <h6 className="dropdown-header">実装資料</h6>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="DB構成の設計（開発者）"
+                >
+                  <i className="fas fa-laptop" /> DB設計資料
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="バックエンドAPIの設計（開発者）"
+                >
+                  <i className="fas fa-laptop" /> WebAPI設計資料
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item link-purple-color"
+                  href="http://www.google.com/"
+                  data-tip="画面の設計（開発者）"
+                >
+                  <i className="fas fa-laptop" /> 画面設計資料
+                  <ReactTooltip effect="float" type="dark" place="bottom" />
+                </a>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
