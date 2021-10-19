a29a0870c51748bf3389f258c7b51b9245e9aeec
Nishimura <yukinkopen@gmail.com>
2021-10-19 22:47:04 +0900

    [mod] 微調整

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,5 +1,29 @@
+import Header from 'components/Header'
+import ReactTooltip from 'react-tooltip'
+
 function Account() {
-  return <div>test</div>
+  return (
+    <div>
+      <Header />
+      <div className="main-top container">
+        <h1
+          data-tip="このページは下記の仕様を満たす必要があります。<br />
+          （仕様なし）
+          "
+          data-for="title"
+        >
+          アカウント設定ページ
+          <ReactTooltip
+            id="title"
+            effect="float"
+            type="dark"
+            place="bottom"
+            multiline={true}
+          />
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default Account
