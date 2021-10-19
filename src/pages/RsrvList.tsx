c2bf06fcdc1a18f1e540393f91acc7e6291bc13f
Nishimura <yukinkopen@gmail.com>
2021-10-19 12:28:03 +0900

    [mod] 予約一覧ページのレイアウトを設定

--- src/pages/RsrvList.tsx
+++ src/pages/RsrvList.tsx
@@ -1,4 +1,5 @@
 import Header from 'components/Header'
+import { Col, Form, ListGroup, Row } from 'react-bootstrap'
 import ReactTooltip from 'react-tooltip'
 
 function RsrvList() {
@@ -25,6 +26,128 @@ function RsrvList() {
             multiline={true}
           />
         </h1>
+        <Form>
+          <Form.Select
+            className="col-6 mt-3 mb-1"
+            aria-label="Default select example"
+          >
+            <option>拠点を選択してください。</option>
+            <option value="1">横浜本社</option>
+            <option value="2">大阪</option>
+            <option value="3">広島</option>
+            <option value="4">福岡</option>
+            <option value="5">仙台</option>
+            <option value="6">札幌</option>
+            <option value="7">高松</option>
+            <option value="8">新潟</option>
+          </Form.Select>
+          <Form.Select
+            className="col-6 mb-1"
+            aria-label="Default select example"
+          >
+            <option>施設を選択してください。</option>
+            <option value="1">2F小会議室A (1,500 円 / 時間)</option>
+            <option value="2">2F小会議室B (1,500 円 / 時間)</option>
+            <option value="3">2F中会議室C (2,500 円 / 時間)</option>
+            <option value="4">3F面談室D (1,000 円 / 時間)</option>
+            <option value="5">3F面談室E (1,000 円 / 時間)</option>
+            <option value="6">4F中会議室F (2,500 円 / 時間)</option>
+            <option value="7">4F大会議室G (3,500 円 / 時間)</option>
+            <option value="8">5F全体ホールH (5,000 円 / 時間)</option>
+          </Form.Select>
+          <Row className="align-items-center">
+            <Col>
+              <Form.Label column lg={2}>
+                予約日
+              </Form.Label>
+            </Col>
+            <Col>
+              <Form.Select
+                className="col-6 mt-3 mb-1"
+                aria-label="Default select example"
+              >
+                <option>年</option>
+                <option value="2021">2021 年</option>
+                <option value="2022">2022 年</option>
+                <option value="2023">2023 年</option>
+              </Form.Select>
+            </Col>
+            <Col>
+              <Form.Select
+                className="col-6 mt-3 mb-1"
+                aria-label="Default select example"
+              >
+                <option>月</option>
+                <option value="1">1 月</option>
+                <option value="2">2 月</option>
+                <option value="3">3 月</option>
+                <option value="4">4 月</option>
+                <option value="5">5 月</option>
+                <option value="6">6 月</option>
+                <option value="7">7 月</option>
+                <option value="8">8 月</option>
+                <option value="9">9 月</option>
+                <option value="10">10 月</option>
+                <option value="11">11 月</option>
+                <option value="12">12 月</option>
+              </Form.Select>
+            </Col>
+            <Col>
+              <Form.Select
+                className="col-6 mt-3 mb-1"
+                aria-label="Default select example"
+              >
+                <option>日</option>
+                <option value="1">1 日</option>
+                <option value="2">2 日</option>
+                <option value="3">3 日</option>
+                <option value="4">4 日</option>
+                <option value="5">5 日</option>
+                <option value="6">6 日</option>
+                <option value="7">7 日</option>
+                <option value="8">8 日</option>
+                <option value="9">9 日</option>
+                <option value="10">10 日</option>
+                <option value="11">11 日</option>
+                <option value="12">12 日</option>
+                <option value="13">13 日</option>
+                <option value="14">14 日</option>
+                <option value="15">15 日</option>
+                <option value="16">16 日</option>
+                <option value="17">17 日</option>
+                <option value="18">18 日</option>
+                <option value="19">19 日</option>
+                <option value="20">20 日</option>
+                <option value="21">21 日</option>
+                <option value="22">22 日</option>
+                <option value="23">23 日</option>
+                <option value="24">24 日</option>
+                <option value="25">25 日</option>
+                <option value="26">26 日</option>
+                <option value="27">27 日</option>
+                <option value="28">28 日</option>
+                <option value="29">29 日</option>
+                <option value="30">30 日</option>
+                <option value="31">31 日</option>
+              </Form.Select>
+            </Col>
+          </Row>
+        </Form>
+        <ListGroup className="mt-5">
+          <h3>〇年〇月〇日　〇〇拠点　〇〇室　予約一覧</h3>
+          <ListGroup.Item>
+            <div>予約者：営業統括部本社営業部　中富太郎（090-2244-7755）</div>
+            <div>予約時間帯：9:00-11:00</div>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <div>予約者：管理部　加藤次郎（090-1234-6644）</div>
+            <div>予約時間帯：13:00-15:00</div>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <div>予約者：システム部　玉出三郎（090-9089-0135）</div>
+            <div>予約時間帯：19:00-21:00</div>
+          </ListGroup.Item>
+        </ListGroup>
       </div>
     </div>
   )
