b03cdfd45c224f9d1a64c12ac5ed7f18e99f8c12
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-16 01:05:28 +0900

    予約作成ページにて入力内容をデータとして保持する形に修正

--- src/pages/MakeRsrv.tsx
+++ src/pages/MakeRsrv.tsx
@@ -1,8 +1,166 @@
+// @ts-nocheck
 import Header from 'components/Header'
 import { Button, Col, Form, Row } from 'react-bootstrap'
 import ReactTooltip from 'react-tooltip'
+import { useState, useEffect } from 'react'
+import { defaultHeaders, callApiGet } from 'common/ApiWrapper'
+import { INVALID_ID } from 'common/Constants'
+
+// TODO 予約者 ID は自分自身であり、ログイン成功時に ID をlocalStorageに保存しておくこと
+
+type Participant = {
+  company_name: string
+  name: string
+}
+
+type Reservation = {
+  hub_id: string
+  facility_id: string
+  title: string
+  tel: string
+  start_year: string
+  start_month: string
+  start_day: string
+  start_hour: string
+  start_minute: string
+  end_year: string
+  end_month: string
+  end_day: string
+  end_hour: string
+  end_minute: string
+  participants: Array<Participant>
+  is_private: boolean
+  notice: string
+}
+
+enum RSRV_TYPE {
+  HubId,
+  FacilityId,
+  Title,
+  Tel,
+  StartYear,
+  StartMonth,
+  StartDay,
+  StartHour,
+  StartMinute,
+  EndYear,
+  EndMonth,
+  EndDay,
+  EndHour,
+  EndMinute,
+  Participants,
+  IsPrivate,
+  Notice,
+}
 
 function MakeRsrv() {
+  const [hubs, setHubs] = useState<Array<Hub>>()
+  const [facilities, setFacilities] = useState<Array<Facility>>()
+
+  const [currentHub, setCurrentHub] = useState<number>(INVALID_ID)
+  const [currentFacility, setCurrentFacility] = useState<string>('')
+  const [currentItem, setItem] = useState<Reservation>({})
+
+  useEffect(() => {
+    // 拠点を取得
+    callApiGet('/hub', defaultHeaders, (res: any) => {
+      const HubList = new Array<Hub>()
+      console.log('hub', res.data)
+      for (const key in res.data) {
+        HubList.push(res.data[key])
+      }
+      setHubs(HubList)
+    })
+  }, [])
+
+  useEffect(() => {
+    // 部署を取得
+    callApiGet('/facility/' + currentHub, defaultHeaders, (res: any) => {
+      const facilityList = new Array<Facility>()
+      console.log('facilities', res.data)
+      for (const key in res.data) {
+        facilityList.push(res.data[key])
+      }
+      setFacilities(facilityList)
+    })
+  }, [currentHub])
+
+  function changeHub(event: any) {
+    console.log('currentHub', event.currentTarget.value)
+    setCurrentHub(event.currentTarget.value)
+  }
+
+  function changeFacility(event: any) {
+    console.log('currentFacility', event.currentTarget.value)
+    setCurrentFacility(event.currentTarget.value)
+  }
+
+  function changeReservation(event: any, type: RSRV_TYPE) {
+    console.log('Reservation data=', event.currentTarget.value)
+    const value = event.currentTarget.value
+    const item = currentItem
+    if (item === null || item === undefined) {
+      console.log('予約データがありません。')
+      return
+    }
+    switch (type) {
+      case RSRV_TYPE.HubId:
+        item.hub_id = value
+        break
+      case RSRV_TYPE.FacilityId:
+        item.facility_id = value
+        break
+      case RSRV_TYPE.Title:
+        item.title = value
+        break
+      case RSRV_TYPE.Tel:
+        item.tel = value
+        break
+      case RSRV_TYPE.StartYear:
+        item.start_year = value
+        break
+      case RSRV_TYPE.StartMonth:
+        item.start_month = value
+        break
+      case RSRV_TYPE.StartDay:
+        item.start_day = value
+        break
+      case RSRV_TYPE.StartHour:
+        item.start_hour = value
+        break
+      case RSRV_TYPE.StartMinute:
+        item.start_minute = value
+        break
+      case RSRV_TYPE.EndYear:
+        item.end_year = value
+        break
+      case RSRV_TYPE.EndMonth:
+        item.end_month = value
+        break
+      case RSRV_TYPE.EndDay:
+        item.end_day = value
+        break
+      case RSRV_TYPE.EndHour:
+        item.end_hour = value
+        break
+      case RSRV_TYPE.EndMinute:
+        item.end_minute = value
+        break
+      case RSRV_TYPE.Participants:
+        item.participants = value
+        break
+      case RSRV_TYPE.IsPrivate:
+        item.is_private = event.currentTarget.checked
+        break
+      case RSRV_TYPE.Notice:
+        item.notice = value
+        break
+      default:
+        break
+    }
+    console.log(item)
+  }
+
   return (
     <div>
       <Header />
@@ -30,38 +188,44 @@ function MakeRsrv() {
         <Form>
           <Form.Select
             className="col-6 mt-3 mb-1"
+            onChange={changeHub}
             aria-label="Default select example"
             data-tip="拠点情報はAPIで取得：getHubs()"
           >
             <option>拠点を選択してください。</option>
-            <option value="1">横浜本社</option>
-            <option value="2">大阪</option>
-            <option value="3">広島</option>
-            <option value="4">福岡</option>
-            <option value="5">仙台</option>
-            <option value="6">札幌</option>
-            <option value="7">高松</option>
-            <option value="8">新潟</option>
+            {hubs &&
+              hubs.map((hub) => {
+                return (
+                  <option key={hub.id} value={hub.id}>
+                    {hub.name}
+                  </option>
+                )
+              })}
           </Form.Select>
           <Form.Select
             className="col-6 mb-1"
+            onChange={changeFacility}
             data-tip="施設情報はAPIで取得：getHubs()で拠点と同時に取得するのが良いかも"
             aria-label="Default select example"
           >
             <option>施設を選択してください。</option>
-            <option value="1">2F小会議室A (1,500 円 / 時間)</option>
-            <option value="2">2F小会議室B (1,500 円 / 時間)</option>
-            <option value="3">2F中会議室C (2,500 円 / 時間)</option>
-            <option value="4">3F面談室D (1,000 円 / 時間)</option>
-            <option value="5">3F面談室E (1,000 円 / 時間)</option>
-            <option value="6">4F中会議室F (2,500 円 / 時間)</option>
-            <option value="7">4F大会議室G (3,500 円 / 時間)</option>
-            <option value="8">5F全体ホールH (5,000 円 / 時間)</option>
+            {facilities?.map((item, index) => {
+              return (
+                <option key={index} value={index}>
+                  {item.name} ({item.hourly_fees.toLocaleString()} 円 / 時間)
+                </option>
+              )
+            })}
           </Form.Select>
           <Form.Text data-tip="ここの料金の部分は選択した施設の値を表示">
             選択した施設の利用料金は、1時間あたり 1,500 円です。
           </Form.Text>
-          <Form.Control className="mt-3" type="text" placeholder="タイトル" />
+          <Form.Control
+            className="mt-3"
+            type="text"
+            placeholder="タイトル"
+            onChange={(e) => changeReservation(e, RSRV_TYPE.Title)}
+          />
           <Row>
             <Col>
               <Form.Label
@@ -77,6 +241,7 @@ function MakeRsrv() {
                 className="mt-3"
                 type="tel"
                 placeholder="電話番号"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.Tel)}
               />
             </Col>
           </Row>
@@ -93,6 +258,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.StartYear)}
                 aria-label="Default select example"
               >
                 <option>年</option>
@@ -104,18 +270,19 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.StartMonth)}
                 aria-label="Default select example"
               >
                 <option>月</option>
-                <option value="1">1 月</option>
-                <option value="2">2 月</option>
-                <option value="3">3 月</option>
-                <option value="4">4 月</option>
-                <option value="5">5 月</option>
-                <option value="6">6 月</option>
-                <option value="7">7 月</option>
-                <option value="8">8 月</option>
-                <option value="9">9 月</option>
+                <option value="01">1 月</option>
+                <option value="02">2 月</option>
+                <option value="03">3 月</option>
+                <option value="04">4 月</option>
+                <option value="05">5 月</option>
+                <option value="06">6 月</option>
+                <option value="07">7 月</option>
+                <option value="08">8 月</option>
+                <option value="09">9 月</option>
                 <option value="10">10 月</option>
                 <option value="11">11 月</option>
                 <option value="12">12 月</option>
@@ -124,18 +291,19 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.StartDay)}
                 aria-label="Default select example"
               >
                 <option>日</option>
-                <option value="1">1 日</option>
-                <option value="2">2 日</option>
-                <option value="3">3 日</option>
-                <option value="4">4 日</option>
-                <option value="5">5 日</option>
-                <option value="6">6 日</option>
-                <option value="7">7 日</option>
-                <option value="8">8 日</option>
-                <option value="9">9 日</option>
+                <option value="01">1 日</option>
+                <option value="02">2 日</option>
+                <option value="03">3 日</option>
+                <option value="04">4 日</option>
+                <option value="05">5 日</option>
+                <option value="06">6 日</option>
+                <option value="07">7 日</option>
+                <option value="08">8 日</option>
+                <option value="09">9 日</option>
                 <option value="10">10 日</option>
                 <option value="11">11 日</option>
                 <option value="12">12 日</option>
@@ -163,11 +331,12 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.StartHour)}
                 aria-label="Default select example"
               >
                 <option>時</option>
-                <option value="8">8 時</option>
-                <option value="9">9 時</option>
+                <option value="08">8 時</option>
+                <option value="09">9 時</option>
                 <option value="10">10 時</option>
                 <option value="11">11 時</option>
                 <option value="12">12 時</option>
@@ -186,6 +355,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.StartMinute)}
                 aria-label="Default select example"
               >
                 <option>分</option>
@@ -209,6 +379,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.EndYear)}
                 aria-label="Default select example"
               >
                 <option>年</option>
@@ -220,6 +391,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.EndMonth)}
                 aria-label="Default select example"
               >
                 <option>月</option>
@@ -240,6 +412,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.EndDay)}
                 aria-label="Default select example"
               >
                 <option>日</option>
@@ -279,6 +452,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.EndHour)}
                 aria-label="Default select example"
               >
                 <option>時</option>
@@ -302,6 +476,7 @@ function MakeRsrv() {
             <Col>
               <Form.Select
                 className="col-6 mt-3 mb-1"
+                onChange={(e) => changeReservation(e, RSRV_TYPE.EndMinute)}
                 aria-label="Default select example"
               >
                 <option>分</option>
@@ -403,12 +578,19 @@ function MakeRsrv() {
             controlId="formHorizontalCheck"
           >
             <Col>
-              <Form.Check label="参加者情報を非公開にする" />
+              <Form.Check
+                label="参加者情報を非公開にする"
+                onClick={(e) => changeReservation(e, RSRV_TYPE.IsPrivate)}
+              />
             </Col>
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>備考欄</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={(e) => changeReservation(e, RSRV_TYPE.Notice)}
+            />
           </Form.Group>
         </Form>
         <Row className="align-items-center mb-5">
