7b528c32c1bf4a0b4a1aa4d0bdc6fea7d14b2023
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-19 22:47:02 +0900

    サーバホストを Heroku に設定

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -1,4 +1,4 @@
-// export const domain = 'https://desolate-gorge-20881.herokuapp.com'
-export const domain = 'http://localhost:3001'
+export const domain = 'https://desolate-gorge-20881.herokuapp.com'
+// export const domain = 'http://localhost:3001'
 
 export const INVALID_ID = -1
